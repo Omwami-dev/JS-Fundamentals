da7d1f746783beb2186cdb6354a4539b64a8332e
Omwami-dev <barasaowabarasa@gmail.com>
2025-07-13 12:54:00 +0300

    created a new file namely 1-multi_languages.js

---
+++ 1-multi_languages.js
@@ -0,0 +1,5 @@
+//A script that writes three lines,with an output of C is fun,
+// python is cool and javascript is amazing
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
